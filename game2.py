f1f1c6bcf2221d581a9eb65b9c0d57c506322133
shimon <123260188+AvarDysania@users.noreply.github.com>
2023-08-05 16:52:11 +0530

    Add files via upload

---
+++ game2.py
@@ -0,0 +1,45 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from pynput.keyboard import Key, Controller
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 750)
+cap.set(4, 520)
+
+detector = HandDetector(detectionCon=0.9, maxHands=1)
+
+keyboard = Controller()
+while True:
+    _, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        fingers = detector.fingersUp(hands[0])
+        if fingers == [0, 0, 0, 0, 0]:
+            keyboard.press(Key.left)
+            keyboard.release(Key.right)
+            keyboard.release(Key.up)
+            keyboard.release(Key.down)
+        elif fingers == [1, 1, 1, 1, 1]:
+            keyboard.press(Key.right)
+            keyboard.release(Key.left)
+            keyboard.release(Key.up)
+            keyboard.release(Key.down)
+        elif fingers == [0, 1, 1, 1, 1]:  # All fingers except the thumb are up
+            keyboard.press(Key.up)
+            keyboard.release(Key.left)
+            keyboard.release(Key.right)
+            keyboard.release(Key.down)
+        elif fingers == [1, 0, 0, 0, 0]:  # Only the thumb is up
+            keyboard.press(Key.down)
+            keyboard.release(Key.left)
+            keyboard.release(Key.right)
+            keyboard.release(Key.up)
+    else:
+        keyboard.release(Key.left)
+        keyboard.release(Key.right)
+        keyboard.release(Key.up)
+        keyboard.release(Key.down)
+
+    cv2.imshow("Shimon Race", img)
+    if cv2.waitKey(1) == ord("q"):
+        break
